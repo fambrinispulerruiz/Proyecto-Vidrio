74878828f2733ee540ad011490725471f266e2a8
giulianoRuiz <87948967+giulianoRuiz@users.noreply.github.com>
2023-09-09 19:20:35 -0300

    ISSUE #5 Agrego Tipos de Vidrio

--- module-simple/src/main/java/domainapp/modules/simple/dom/vidrio/TipoVidrio.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/vidrio/TipoVidrio.java
@@ -2,6 +2,15 @@ package domainapp.modules.simple.dom.vidrio;
 
 public enum TipoVidrio {
 
-	Parabrisa;
+	Parabrisa,
+	Luneta,
+	Puerta Delantera D,
+	Puerta Delantera I,
+	Puerta Trasera D,
+	Puerta Trasera I,
+	Espejo Retrovisor,
+	Espejo Lateral D,
+	Espejo Lateral I
+	;
 	
 }
